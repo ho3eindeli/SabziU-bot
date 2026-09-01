8d73962854d04c082e7915b4dcbe6d670a0367ec
ho3eindeli <hosseindelivand@gmail.com>
2026-09-01 14:29:12 +0330

    Update bale_bot.py

--- bale_bot.py
+++ bale_bot.py
@@ -2314,21 +2314,21 @@ async def process_payment_receipt(
 
 @bot.event
 async def on_message(
-    message: Message
+   message: Message
 ):
 
    user_id = str(
-    message.author.user_id
-)
+       message.author.user_id
+    )
 
-print("=" * 60, flush=True)
-print("🔎 MESSAGE RECEIVED", flush=True)
-print(f"👤 USER_ID: {user_id}", flush=True)
-print(f"👤 AUTHOR: {message.author}", flush=True)
-print(f"💬 CHAT: {message.chat}", flush=True)
-print(f"💬 CHAT_ID: {getattr(message.chat, 'id', None)}", flush=True)
-print(f"📝 CONTENT: {message.content}", flush=True)
-print("=" * 60, flush=True)
+    print("=" * 60, flush=True)
+    print("🔎 MESSAGE RECEIVED", flush=True)
+    print(f"👤 USER_ID: {user_id}", flush=True)
+    print(f"👤 AUTHOR: {message.author}", flush=True)
+    print(f"💬 CHAT: {message.chat}", flush=True)
+    print(f"💬 CHAT_ID: {getattr(message.chat, 'id', None)}", flush=True)
+    print(f"📝 CONTENT: {message.content}", flush=True)
+    print("=" * 60, flush=True)
     # =====================================================
     # /start
     # =====================================================
